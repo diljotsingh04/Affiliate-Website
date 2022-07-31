d96b00bbef62af04e7a92af2d0f979163b5c1848
Diljot Singh <singhdiljot2306@gmail.com>
2022-07-31 20:14:13 +0530

    fetching data fromm api

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <>
+        <nav style={{"position": "fixed"}}>
+            <div className="logo"><a href="/"  style={{"text-decoration": "none", "color": "white"}}> Diljot S </a></div>
+            <input type="checkbox" id="click"/>
+            <label htmlFor="click" className="menu-btn">
+            <i >=</i>
+            
+            </label>
+            <ul>
+              <li><a className="active" href="/">Home</a></li>
+              <li><a href="/">About</a></li>
+              <li><a href="/">Services</a></li>
+              <li><a href="/">Gallery</a></li> 
+              <li><a href="/">Feedback</a></li>
+            </ul>
+        </nav> 
+    </>
+  )
+}
+
+export default Navbar;
