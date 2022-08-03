ae60842243ed25388c11534938560c2bc563578d
Diljot Singh <singhdiljot2306@gmail.com>
2022-08-03 13:49:45 +0530

    added filter with select options

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,33 +3,57 @@ import proItem from '../api.json'
 import Allcards from "./Allcards";
 
 const Card = () => {
-
+    //use state
     const [defItem, setdefItem] = useState(proItem);
 
+    // filter function
     const filterItem = (category) =>{
+        
+
         const updatedList = proItem.filter((curEle) =>{
             return curEle.category === category;
         });
-        console.log(updatedList);
         setdefItem(updatedList);
+    }
+
+    const getItem = () =>{
+        
+        var e = document.getElementById("filItem");
+        // var value = e.value;
+        var text = e.options[e.selectedIndex].text;
+        
+        // console.log(text.toLowerCase());
+
+        if(text.toLowerCase() === 'all'){
+            setdefItem(proItem)
+        }
+        else{
+            filterItem(text.toLowerCase());
+        }
     }
 
     return (
         <>
+            <div className="filItem">
+                <label htmlFor="cars">Filter Items: &nbsp;</label>
+                <select name="cars" id='filItem' onChange={()=> getItem()}>
+                    <option>All</option>
+                    <option>Shoes</option>
+                    <option>Shirt</option>
+                    <option>Mugs</option>
+                </select>
+            </div>
+
+
             <Allcards proItem={defItem}/>
-            <label for="cars">Filter Items:</label>
-            <select name="cars" id="cars">
-                <option value="All"  onClick={() => setdefItem(proItem)}>All</option>
-                <option value="Shoes"  onClick={() => filterItem("mugs")}>Shoes</option>
-                <option value="Shirts">Shirts</option>
-                <option value="Mugs">Mugs</option>
-            </select>
-            <ul>
+
+            {/* rejected list*/}
+            {/* <ul>
                 <li><button onClick={() => setdefItem(proItem)}>All</button></li>
                 <li><button onClick={() => filterItem("mugs")}>Mugs</button></li>
                 <li><button onClick={() => filterItem("shoes")}>Shoes</button></li>
                 <li><button onClick={() => filterItem("shirt")}>Shirts</button></li>
-            </ul>
+            </ul> */}
         </>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <>
-        <nav style={{"position": "fixed"}}>
+        <nav style={{"position": "sticky"}}>
             <div className="logo"><a href="/"  style={{"textDecoration": "none", "color": "white"}}> Diljot S </a></div>
             <input type="checkbox" id="click"/>
             <label htmlFor="click" className="menu-btn">
@@ -13,10 +13,10 @@ const Navbar = () => {
             </label>
             <ul>
               <li><a className="active" href="/">Home</a></li>
-              <li><a href="/">About</a></li>
-              <li><a href="/">Services</a></li>
-              <li><a href="/">Gallery</a></li> 
-              <li><a href="/">Feedback</a></li>
+              <li><a href="/" style={{"pointerEvents": "none"}}>About</a></li>
+              <li><a href="/" style={{"pointerEvents": "none"}}>Services</a></li>
+              <li><a href="/" style={{"pointerEvents": "none"}}>Gallery</a></li> 
+              <li><a href="/" style={{"pointerEvents": "none"}}>Feedback</a></li>
             </ul>
         </nav> 
     </>
